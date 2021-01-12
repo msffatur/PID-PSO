532f7e4a581dc681a15c5a2dc3f7ed14688a5efa
msffatur <63286645+msffatur@users.noreply.github.com>
2021-01-13 05:51:22 +0700

    Update pick_reference.m

--- pick_reference.m
+++ pick_reference.m
@@ -1,8 +1,8 @@
 function [a,v,s] = pick_reference(numref)
 
-acc = 'percepatan';
-spd = 'kecepatan';
-dis = 'jarak';
+acc = 'acceleration';
+spd = 'speed';
+dis = 'position';
 format = '.mat';
 a = append(acc,string(numref),format);
 v = append(spd,string(numref),format);
